a4478494c9d94345e3ae28d2469c1ad64f254aa0
Vivek C <c_vivek@rediffmail.com>
2019-06-29 21:38:35 +0530

    Bringing ModbusPoll to source control. Builds but does not link yet.

---
+++ ModbusPoll/stdafx.cpp
@@ -0,0 +1,16 @@
+
+// stdafx.cpp : source file that includes just the standard includes
+// Modbus Poll.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+/*Modbus Poll Places to be improved or optimized
+1>Data format display, you should use a method or data structure to transform each other
+1.1 The original data can be considered Short Or unsigned short
+1.2 Signed,Unsigned,Hex,Binary£¬can be directly converted to each other
+1.3 Long, Float, Double
+2>Replace this Grid with no registration to List Operation
+3>Display the communication settings in the status bar
+*/
+ 
